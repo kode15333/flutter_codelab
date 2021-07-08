6228674511caa6dca7c26c03cc304028be3e8f91
JunhoYou <youjh@rsupport.com>
2021-07-08 21:17:40 +0900

    feat: widget animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/widge_animation.dart';
 import 'page_animation.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Page1(),
+      home: PhysicsCardDragDemo(),
     ),
   );
 }
