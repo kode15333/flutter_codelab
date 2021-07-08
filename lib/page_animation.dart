4e67150a0be1fa2e1a76f1c00119202e6b903cbc
JunhoYou <youjh@rsupport.com>
2021-07-08 20:33:31 +0900

    style: animation 추가

--- lib/page_animation.dart
+++ lib/page_animation.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
-
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },
-          child: Text("GO!"),
-        ),
-      )
-    );
+        appBar: AppBar(),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(_createRoute());
+            },
+            child: Text("GO!"),
+          ),
+        ));
   }
 }
 
@@ -22,7 +20,12 @@ Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Page2(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      return SlideTransition(position: animation.drive(tween), child: child);
     },
   );
 }
@@ -38,4 +41,3 @@ class Page2 extends StatelessWidget {
     );
   }
 }
-
