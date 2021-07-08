53c6068d01aa8d9aab7e46042b0a0fb95ac847b9
JunhoYou <youjh@rsupport.com>
2021-07-08 20:26:23 +0900

    fix: Page1 import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hello_flutter/page_animation.dart';
+import 'page_animation.dart';
 void main() {
-  runApp(Page1());
+  runApp(
+    MaterialApp(
+      home: Page1(),
+    ),
+  );
 }
